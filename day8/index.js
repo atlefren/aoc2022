2a6973c388a03914b4b906fbdf78a1fe14656c11
Atle Frenvik Sveen <atle.sveen@gmail.com>
2022-12-08 13:00:09 +0100

    refactor

--- day8/index.js
+++ day8/index.js
@@ -18,31 +18,18 @@ const getSides = (matrix, x, y) => {
 const isVisible = (value, x, y, matrix) =>
   getSides(matrix, x, y).some((side) => side.every((e) => value > e));
 
-const getVisible = (input) =>
-  input.reduce(
-    (acc, l, x) => [
-      ...acc,
-      ...l
-        .map((value, y) => ({
-          x,
-          y,
-          value,
-        }))
-        .filter(({ y, x, value }) => isVisible(value, x, y, input)),
-    ],
+const loopMatrix = (matrix, func) =>
+  matrix.reduce(
+    (acc, l, x) => [...acc, ...l.map((value, y) => func(value, x, y, matrix))],
     []
   );
 
-const countVisible = (val, line) => {
-  let c = 0;
-  for (let i = 0; i < line.length; i++) {
-    c++;
-    if (line[i] >= val) {
-      return c;
-    }
-  }
-  return c;
-};
+const countVisible = (val, line) =>
+  line.reduce(
+    (acc, value) =>
+      acc.stopped ? acc : { stopped: value >= val, count: acc.count + 1 },
+    { count: 0, stopped: false }
+  ).count;
 
 const getScenicScore = (value, x, y, matrix) =>
   getSides(matrix, x, y).reduce(
@@ -50,17 +37,15 @@ const getScenicScore = (value, x, y, matrix) =>
     1
   );
 
-const getScenicScores = (input) =>
-  input.reduce(
-    (acc, l, x) => [
-      ...acc,
-      ...l.map((value, y) => getScenicScore(value, x, y, input)),
-    ],
-    []
-  );
+const task1 = (input) =>
+  loopMatrix(input, (value, x, y, matrix) => ({
+    x,
+    y,
+    value,
+    matrix,
+  })).filter(({ y, x, value, matrix }) => isVisible(value, x, y, matrix))
+    .length;
 
-const task1 = (input) => getVisible(input).length;
-
-const task2 = (input) => Math.max(...getScenicScores(input));
+const task2 = (input) => Math.max(...loopMatrix(input, getScenicScore));
 
 run(parse, task1, task2, true);
